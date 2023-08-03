59c2af224666f88047d52f826d918590e8a3a410
Abraham Cabrera <ingeabraham23@gmail.com>
2023-08-03 15:28:52 -0600

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,9 @@ import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <ToastContainer autoClose={1000} position="bottom-right"/>
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
