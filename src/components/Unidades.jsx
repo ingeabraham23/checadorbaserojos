f724c60a5026e1127864c6903016436bfc360bc7
Abraham Cabrera <ingeabraham23@gmail.com>
2023-08-04 09:06:40 -0600

    Va de nuevo PWA

--- src/components/Unidades.jsx
+++ src/components/Unidades.jsx
@@ -29,6 +29,20 @@ function Unidades() {
 
   const numeroInputRef = useRef(null);
 
+  const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+}, []);
+
   useEffect(() => {
     const checkAlarmTepepan = () => {
       const currentDate = new Date();
@@ -542,8 +556,30 @@ function Unidades() {
     }
   };
 
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
   return (
     <div className="unidades-container">
+      {isReadyForInstall && <button onClick={downloadApp}>
+        Descarga la App</button>}
       {!showForm && (
         <div className="add-button-container">
           <button className="add-button" onClick={handleAddUnidad}>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,24 @@ import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js"
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <ToastContainer autoClose={1000} position="bottom-right"/>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({type: "SKIP_WAITING"});
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
